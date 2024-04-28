c4672c3e24d19730f1edce3ad16c23b0d70744a9
James Murphy <james.m.murphy@gmail.com>
2024-04-27 22:01:19 -0400

    Initial version

---
+++ src/JamesQMurphy.Extensions/JamesQMurphy.Extensions/Extensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace JamesQMurphy.Extensions
+{
+    public static class Extensions
+    {
+        public static string ToBase64UrlString(this byte[] inArray)
+        {
+            return Convert.ToBase64String(inArray)
+                .Replace("=", String.Empty)
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
+
+        public static string ToMiniGuid(this Guid guid)
+        {
+            return guid.ToByteArray().ToBase64UrlString();
+        }
+
+        public static string ToIso8601(this DateTime dateTime)
+        {
+            return DateTime
+                .SpecifyKind(dateTime, DateTimeKind.Utc)
+                .ToString("O");
+        }
+
+        // Thank you Xav987!
+        // https://stackoverflow.com/a/18828085/1001100
+        public static bool IsSubClassOfGeneric(this Type child, Type parent)
+        {
+            if (child == parent)
+                return false;
+
+            if (child.IsSubclassOf(parent))
+                return true;
+
+            var parameters = parent.GetGenericArguments();
+            var isParameterLessGeneric = !(parameters != null && parameters.Length > 0 &&
+                ((parameters[0].Attributes & TypeAttributes.BeforeFieldInit) == TypeAttributes.BeforeFieldInit));
+
+            while (child != null && child != typeof(object))
+            {
+                var cur = GetFullTypeDefinition(child);
+                if (parent == cur || (isParameterLessGeneric && cur.GetInterfaces().Select(i => GetFullTypeDefinition(i)).Contains(GetFullTypeDefinition(parent))))
+                    return true;
+                else if (!isParameterLessGeneric)
+                    if (GetFullTypeDefinition(parent) == cur && !cur.IsInterface)
+                    {
+                        if (VerifyGenericArguments(GetFullTypeDefinition(parent), cur))
+                            if (VerifyGenericArguments(parent, child))
+                                return true;
+                    }
+                    else
+                        foreach (var item in child.GetInterfaces().Where(i => GetFullTypeDefinition(parent) == GetFullTypeDefinition(i)))
+                            if (VerifyGenericArguments(parent, item))
+                                return true;
+
+                child = child.BaseType;
+            }
+
+            return false;
+        }
+
+        private static Type GetFullTypeDefinition(Type type)
+        {
+            return type.IsGenericType ? type.GetGenericTypeDefinition() : type;
+        }
+
+        private static bool VerifyGenericArguments(Type parent, Type child)
+        {
+            Type[] childArguments = child.GetGenericArguments();
+            Type[] parentArguments = parent.GetGenericArguments();
+            if (childArguments.Length == parentArguments.Length)
+                for (int i = 0; i < childArguments.Length; i++)
+                    if (childArguments[i].Assembly != parentArguments[i].Assembly || childArguments[i].Name != parentArguments[i].Name || childArguments[i].Namespace != parentArguments[i].Namespace)
+                        if (!childArguments[i].IsSubclassOf(parentArguments[i]))
+                            return false;
+
+            return true;
+        }
+    }
+}
---
+++ src/JamesQMurphy.Extensions/JamesQMurphy.Extensions/README.md
@@ -0,0 +1,3 @@
+# JamesQMurphy.Extensions
+
+A handful of useful .NET extensions.  You can browse the source code on [GitHub](https://github.com/JamesQMurphy/Extensions).
